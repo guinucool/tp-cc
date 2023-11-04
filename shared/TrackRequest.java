f65c5b14c7cd5faa7307f70f70e5d91fed88594f
Skyrush <tomasms03@gmail.com>
2023-11-04 16:41:47 +0000

    [shared] Gets e setters de Tracks Response e Request

--- shared/TrackRequest.java
+++ shared/TrackRequest.java
@@ -1,5 +1,33 @@
 package shared;
 
 public class TrackRequest {
-    
+
+    public enum Command {
+        REGS,
+        GET
+    }
+
+    private Command cmd;
+    private String args;
+
+    public TrackRequest(Command cmd, String args) {
+        this.cmd = cmd;
+        this.args = args;
+    }
+
+    public Command getCommand() {
+        return this.cmd;
+    }
+
+    public String getArguments() {
+        return this.args;
+    }   
+
+    public Command setCommand(Command cmd) {
+        return this.cmd = cmd;
+    }
+
+    public String setArguments(String args) {
+        return this.args = args;
+    }  
 }
--- shared/TrackResponse.java
+++ shared/TrackResponse.java
@@ -2,4 +2,33 @@ package shared;
 
 public class TrackResponse {
     
+    public enum Code {
+        FAIL,
+        SUC    
+    }
+    
+    private Code code;
+    private String args;
+
+    public TrackResponse(Code code, String args) {
+        this.code = code;
+        this.args = args;
+    }
+
+    public Code getCode() {
+        return this.code;
+    }
+
+    public String getArguments() {
+        return this.args;
+    }  
+    
+    public Code setCode(Code code) {
+        return this.code = code;
+    }
+
+    public String setArguments(String args) {
+        return this.args = args;
+    }   
 }
+
