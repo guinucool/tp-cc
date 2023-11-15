95a4fe61b8afaa00e248ca38d10164fa1df2cb1b
Skyrush <tomasms03@gmail.com>
2023-11-15 22:26:23 +0000

    [FSBlock e TrackerFile] adição de getters e setters e funcoes adicionais

--- src/files/FSBlock.java
+++ src/files/FSBlock.java
@@ -1,7 +1,79 @@
 import java.util.*;
 
+/**
+ * Objeto que regista, na generalidade dos casos, um ficheiro do sistema.
+ * 
+ * @param offset
+ *      O número do bloco
+ * 
+ * @param nodes
+ *      A lista de todos os nodes que possuem o block offset
+ */
 public class FSBlock {
     
     private int offset;
     private List<Node> nodes;
+
+    /* Constructors */
+    public FSBlock(){
+        this.offset = 0;
+        this.nodes = new ArrayList<Node>();
+    }
+
+    public FSBlock(int offset,List<Node> nodes){
+        this.setOffset(offset);
+        this.setNodes(nodes);
+    }
+
+    public FSBlock(FSBlock fsblock){
+        this.setOffset(fsblock.offset);
+        this.setNodes(fsblock.nodes);
+    }
+
+    /* Setters */
+    public void setOffset(int offset){
+        this.offset = offset;
+    }
+
+    public void setNodes(List<Node> nodes){
+        this.nodes.removeAll(this.nodes);
+        this.nodes = new ArrayList<Node>();
+        for(Node a: nodes)
+            this.nodes.add(a);
+    }
+
+    /* Getters */
+    public int getOffset(){
+        return this.offset;
+    }
+
+    public List<Node> getNodes(){
+        ArrayList<Node> res = new ArrayList<Node>();
+        for(Node a: this.nodes)
+            res.add(a);
+
+        return res;
+    }
+
+    /* Auxiliar */
+
+    public void addNode(Node node){
+        this.nodes.add(node);
+    }
+
+    public boolean equals(Object o) {
+
+        if (this == o)
+            return true;
+
+        if ((o == null) || (this.getClass() != o.getClass()))
+            return false;
+
+        FSBlock fsblock = (FSBlock) o;
+        return this.offset == fsblock.offset && this.nodes.equals(fsblock.nodes);
+    }
+
+    public Object clone() {
+        return new FSBlock(this);
+    }
 }
--- src/files/TrackerFile.java
+++ src/files/TrackerFile.java
@@ -4,4 +4,67 @@ public class TrackerFile {
     
     private long blocksize;
     private List<FSBlock> blocks;
+
+    /* Constructors */
+    public TrackerFile(){
+        this.blocksize = 0;
+        this.blocks = new ArrayList<FSBlock>();
+    }
+
+    public TrackerFile(long blocksize, List<FSBlock> blocks){
+        this.setBlocksize(blocksize);
+        this.setBlocks(blocks);
+    }
+
+    public TrackerFile(TrackerFile tf){
+        this.setBlocksize(tf.blocksize);
+        this.setBlocks(tf.blocks);
+    }
+
+    /* Setters */
+    public void setBlocksize(long blocksize){
+        this.blocksize = blocksize;
+    }
+
+    public void setBlocks(List<FSBlock> blocks){
+        this.blocks.removeAll(this.blocks);
+        this.blocks = new ArrayList<FSBlock>();
+        for(FSBlock a: blocks)
+            this.blocks.add(a);
+    }
+
+    /* Getters */
+    public long getOffset(){
+        return this.blocksize;
+    }
+
+    public List<FSBlock> getNodes(){
+        ArrayList<FSBlock> res = new ArrayList<FSBlock>();
+        for(FSBlock a: this.blocks)
+            res.add(a);
+
+        return res;
+    }
+
+    /* Auxiliar */
+
+    public void addNode(FSBlock block){
+        this.blocks.add(block);
+    }
+
+    public boolean equals(Object o) {
+
+        if (this == o)
+            return true;
+
+        if ((o == null) || (this.getClass() != o.getClass()))
+            return false;
+
+        TrackerFile tf = (TrackerFile) o;
+        return this.blocksize == tf.blocksize && this.blocks.equals(tf.blocks);
+    }
+
+    public Object clone() {
+        return new TrackerFile(this);
+    }
 }
