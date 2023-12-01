2fdbe3b37f1881058edd833180fd900f738501ef
guinucool <guinucool@gmail.com>
2023-12-01 20:54:12 +0000

    TrackFile completa.

---
+++ source/file/BlockFile.java
@@ -0,0 +1,185 @@
+package file;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+/**
+ * Objeto que define a estrutura de um ficheiro que pode ser divido em blocos
+ * de x bytes.
+ */
+public abstract class BlockFile extends FSFile {
+    
+    public final int MAX_BLOCK = 8192;          /* Número máximo de blocos possível */
+
+    private int blocksize;          /* Tamanho de bloco definido para o ficheiro */
+
+    /* Construtor vazio */
+    public BlockFile() {
+        super();
+        this.blocksize = 0;
+    }
+
+    /* Construtor parametrizado */
+    public BlockFile(String hash, String name, long size) throws FileException {
+        super(hash, name, size);
+        this.setBlocksize();
+    }
+
+    /* Construtor parametrizado com blocksize pré-definido */
+    public BlockFile(String hash, String name, long size, int blocksize) throws FileException {
+        super(hash, name, size);
+        this.setBlocksize(blocksize);
+    }
+
+    /* Construtor de cópia */
+    public BlockFile(BlockFile file){
+        super(file);
+        this.blocksize = file.blocksize;
+    }
+
+    /* Construtor binário */
+    public BlockFile(byte[] data, boolean blocksize) throws FileException {
+        /* Constroi o file primeiro */
+        super(data);
+
+        /* Vai buscar o blocksize, se existir */
+        if (blocksize) {
+
+            ByteArrayInputStream barray = new ByteArrayInputStream(data);
+            DataInputStream stream = new DataInputStream(barray);
+
+            /* Conversão binária para node */
+            try {
+                stream.readUTF();
+                stream.readUTF();
+                stream.readLong();
+                this.setBlocksize(stream.readInt());
+
+            } catch (IOException e) {
+                throw new FileException("file-invalid");
+            }
+
+        } else {
+            this.setBlocksize();
+        }
+    }
+
+    /* Define qual vai ser o tamanho dos blocos dependendo das propriedades do ficheiro */
+    private void setBlocksize() {
+
+        /* Define o número máximo de blocos para o ficheiro */
+        int max = MAX_BLOCK;
+        while (super.getSize() < max * 50 && max > 8)
+            max /= 2;
+
+        /* Encontra o tamanho de bloco mais eficiente */
+        int blocksize = 1;
+	    long num = Long.MAX_VALUE;
+
+	    while(num > max) {
+
+	        num = this.getSize() / blocksize;
+
+            if (this.getSize() % blocksize != 0)
+                num++;
+
+            blocksize *= 2;
+	    }
+
+	    this.blocksize = blocksize / 2;
+    }
+
+    /* Define o tamanho de um bloco do ficheiro com um valor já pré-definido */
+    private void setBlocksize(int blocksize) throws FileException {
+
+        /* Descobre o máximo para este ficheiro */
+        int max = this.getMaxBlocks();
+
+        if (super.getSize() % blocksize != 0)
+            max--;
+
+        /* Verifica se o blocksize fornecido é válido */
+        if (blocksize < 1 || super.getSize() / blocksize > max)
+            throw new FileException("blocksize-invalid");
+
+        this.blocksize = blocksize;
+    }
+
+    /* Devolve o tamanho definido para blocos deste ficheiro */
+    public int getBlocksize() {
+        return this.blocksize;
+    }
+
+    /* Devolve o tamanho do último bloco do ficheiro (pode ser igual aos outros) */
+    public int getLastBlocksize() {
+
+        int size = (int) super.getSize() % this.blocksize;
+
+        if (size == 0)
+            size = this.getBlocksize();
+
+        return size;
+    }
+
+    /* Calcula o número de blocos que um ficheiro tem */
+    public int getNumBlocks() {
+
+        int num = (int) (super.getSize() / this.blocksize);
+
+        if(super.getSize() % this.blocksize != 0)
+            num++;
+
+        return num;
+    }
+
+    /* Calcula o número máximo de blocos que um ficheiro pode ter */
+    public int getMaxBlocks() {
+
+        /* Define o número máximo de blocos para o ficheiro */
+        int max = MAX_BLOCK;
+        while (super.getSize() < max * 50 && max > 8)
+            max /= 2;
+
+        return max;
+    }
+
+    /* Converte o ficheiro em formato binário */
+    public byte[] getBytes() throws FileException {
+
+        ByteArrayOutputStream barray = new ByteArrayOutputStream();
+        DataOutputStream stream = new DataOutputStream(barray);
+        
+        try {
+            stream.write(super.getBytes());
+            stream.writeInt(this.blocksize);
+
+            stream.flush();
+
+            return barray.toByteArray();
+
+        } catch (IOException e) {
+            throw new FileException("file-outofmemory");
+        }
+    }
+
+    /* Verifica se um objeto é igual ao ficheiro */
+    public boolean equals(Object o) {
+
+        BlockFile file = (BlockFile) o;
+        return super.equals(o) && this.blocksize == file.blocksize;
+    }
+
+    /* Converte o ficheiro para string */
+    public String toString() {
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("(Block)").append(super.toString());
+        builder.append("blocksize:").append(this.blocksize).append(";");
+
+        return builder.toString();
+    }
+}
--- source/file/TrackFile.java
+++ source/file/TrackFile.java
@@ -1,7 +1,146 @@
 package file;
 
-public class TrackFile extends FSFile {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-    private int blocksize;
-    
+import file.block.Block;
+import file.block.BlockException;
+import model.Node;
+
+/**
+ * Objeto que define a estrutura do registo de um ficheiro no tracker.
+ */
+public class TrackFile extends BlockFile {
+
+    private Map<Integer,Block> blocks;          /* Mapa que armazena blocos e os nodes associados */
+
+    /* Construtor vazio */
+    public TrackFile() {
+        super();
+        this.blocks = new HashMap<>();
+    }
+
+    /* Construtor parametrizado */
+    public TrackFile(String hash, String name, long size) throws FileException {
+        super(hash, name, size);
+        this.setBlocks();
+    }
+
+    /* Construtor de cópia */
+    public TrackFile(TrackFile file) {
+        super(file);
+        this.blocks = new HashMap<>();
+
+        /* Popula o novo hash map */
+        for (Block block : file.getBlocks())
+            this.blocks.put(block.getOffset(), block);
+    }
+
+    /* Construtor binário */
+    public TrackFile(byte[] data) throws FileException {
+        super(data, false);
+        this.setBlocks();
+    }
+
+    /* Define um mapa de blocos que não têm nenhum node associdado */
+    private void setBlocks() {
+
+        /* Cria o hash map que vai armazenar os blocos */
+        this.blocks = new HashMap<>();
+
+        /* Descobre a quantidade de blocos */
+        int size = this.getNumBlocks();
+
+        /* Vai criando e armazenando os vários blocos */
+        for (int i = 0; i < size; i++) {
+            try {
+                this.blocks.put(i, new Block(i));
+            } catch (BlockException e) {
+                /* Não é preciso tratar esta exceção */
+            }
+        }
+    }
+
+    /* Adiciona um nodo a todos os blocos do ficheiro */
+    public void addNode(Node node) throws BlockException {
+        for (Block block : this.blocks.values())
+            block.addNode(node);
+    }
+
+    /* Adiciona um nodo a um bloco em específico do ficheiro */
+    public void addNode(int offset, Node node) throws BlockException, FileException {
+        if (offset >= this.blocks.size() || offset < 0)
+            throw new FileException("offset-invalid");
+
+        this.blocks.get(offset).addNode(node);
+    }
+
+    /* Remove um nodo de todos os blocos do ficheiro */
+    public void removeNode(Node node) {
+        for (Block block : this.blocks.values())
+            block.removeNode(node);
+    }
+
+    /* Devolve o número de blocos que um ficheiro tem */
+    public int getNumBlocks() {
+
+        /* Caso a lista blocos já tenha sido gerada */
+        if (this.blocks != null && this.blocks.size() != 0)
+            return this.blocks.size();
+
+        /* Caso contrário calcula o tamanho */
+        return super.getNumBlocks();
+    }
+
+    /* Devolve um bloco em específico do ficheiro */
+    public Block getBlock(int offset) throws FileException {
+
+        if (offset >= this.blocks.size() || offset < 0)
+            throw new FileException("offset-invalid");
+
+        return (Block) this.blocks.get(offset).clone();
+    }
+
+    /* Devolve a lista de blocos que um ficheiro tem */
+    public List<Block> getBlocks() {
+
+        List<Block> blocklist = new ArrayList<Block>();
+
+        for(Block block: this.blocks.values())
+            blocklist.add((Block) block.clone());
+
+        return blocklist;
+    }
+
+    /* Verifica se um objeto é igual ao ficheiro track */
+    public boolean equals(Object o) {
+
+        TrackFile file = (TrackFile) o;
+        return super.equals(o) && this.blocks.equals(file.blocks);
+    }
+
+    /* Clona o ficheiro criando um novo idêntico */
+    public Object clone() {
+        return new TrackFile(this);
+    }
+
+    /* Converte o ficheiro para string */
+    public String toString() {
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("(Track)").append(super.toString());
+        builder.append("blocks:");
+
+        for (Map.Entry<Integer,Block> block : this.blocks.entrySet()) {
+            builder.append(" ").append("key: ").append(block.getKey());
+            builder.append(" ").append("value: ").append(block.getValue().toString());
+        }
+
+        builder.append(";");
+
+        return builder.toString();
+    }
 }
